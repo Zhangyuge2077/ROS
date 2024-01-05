4fb29781f00c399641fdc2a20e7a99925875dce8
Zhangyuge2077 <15328362837@163.com>
2024-01-05 20:02:47 +0800

    first commit

---
+++ ros_ws/build/task1/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;sensor_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ltask1".split(';') if "-ltask1" != "" else []
+PROJECT_NAME = "task1"
+PROJECT_SPACE_DIR = "/mnt/ros_ws/install"
+PROJECT_VERSION = "0.0.0"
